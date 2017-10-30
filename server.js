7924e6750ac8c2d2e0b2a6bbda211fe8200eeb29
Colton Cmar <colton@David-Silbermanns-MacBook-Pro.local>
2017-10-30 19:08:28 -0400

    mongo promise change

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost/test", {
   useMongoClient: true
 });
 
